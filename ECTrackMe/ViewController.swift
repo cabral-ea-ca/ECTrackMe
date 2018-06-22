ecb0f497517bca6d3e729117aee23066526e23ed
Ryerson Student <ryersonstudent@ryersons-mac.local>
2018-06-21 22:38:29 -0400

    Initial Commit

---
+++ ECTrackMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECTrackMe
+//
+//  Created by Ryerson Student on 2018-06-21.
+//  Copyright © 2018 Ryerson Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
